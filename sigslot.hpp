4b10fa4d4209b3c123aab6ebaa9ca90623a0a483
Carl Bao <xiaopeng@anlantech.com>
2020-01-02 17:18:30 +0800

    refactor the trackable

--- sigslot.hpp
+++ sigslot.hpp
@@ -1,51 +1,180 @@
 
+#pragma once
+
+#include <memory>
 #include <functional>
 #include <list>
-#include <memory>
 
-struct trackable
+namespace utils
 {
-    static std::shared_ptr<trackable> get_base_checkable()
-    {
-        static std::shared_ptr<trackable> base_checkable(std::make_shared<trackable>());
-        return base_checkable;
-    }
+
+namespace detail
+{
+
+template <typename SharedPtr>
+struct shared_ptr_traits
+{
 };
 
-template <typename... _ArgTypes>
-class sigslot
+template <typename T>
+struct shared_ptr_traits<std::shared_ptr<T>>
+{
+    typedef std::weak_ptr<T> weak_type;
+};
+
+struct shared_ptr_continer_base
+{
+    virtual operator bool() = 0;
+};
+
+template <typename SharedPtrType>
+class shared_ptr_continer_imp : public shared_ptr_continer_base
 {
 public:
-    using SlotFunc = std::function<void(_ArgTypes...)>;
-    using SlotID = std::weak_ptr<SlotFunc>;
-    using SlotType = std::pair<std::shared_ptr<SlotFunc>, std::weak_ptr<trackable>>;
-    SlotID connect(SlotFunc func, std::weak_ptr<trackable> track = trackable::get_base_checkable())
+    shared_ptr_continer_imp(SharedPtrType const &shared)
+        : m_shared(shared)
     {
-        auto const f = std::make_shared<SlotFunc>(std::move(func));
-        m_funcs.push_back(std::make_pair(f, track));
-        return f;
+    }
+    operator bool()
+    {
+        return (bool)m_shared;
     }
 
-    void disconnect(SlotID f)
+private:
+    SharedPtrType m_shared;
+};
+
+class shared_ptr_continer
+{
+public:
+    shared_ptr_continer() {}
+    template <typename SharedPtrType>
+    shared_ptr_continer(SharedPtrType const &shared)
+        : m_imp(std::make_unique<shared_ptr_continer_imp<SharedPtrType>>(shared))
     {
-        m_funcs.remove_if([f](SlotType slot) {
-            return slot.first == f.lock();
+    }
+
+    operator bool()
+    {
+        return m_imp && m_imp->operator bool();
+    }
+
+private:
+    std::unique_ptr<shared_ptr_continer_base> m_imp;
+};
+
+struct weak_ptr_continer_base
+{
+    virtual shared_ptr_continer lock() const = 0;
+};
+
+template <typename WeakPtrType>
+class weak_ptr_continer_imp : public weak_ptr_continer_base
+{
+public:
+    weak_ptr_continer_imp(WeakPtrType const &weak) : m_weak(weak)
+    {
+    }
+    virtual shared_ptr_continer lock() const
+    {
+        return shared_ptr_continer(m_weak.lock());
+    }
+
+private:
+    WeakPtrType m_weak;
+};
+
+template <typename... signature>
+class slot;
+
+template <typename R, typename... ArgTypes>
+class slot<R(ArgTypes...)>
+{
+public:
+    using SlotFunc = std::function<R(ArgTypes...)>;
+
+    slot(SlotFunc const &func) : m_func(func)
+    {
+    }
+
+    slot(slot &other)
+    {
+        m_weak.swap(other.m_weak);
+        m_tracked = other.m_tracked;
+        m_func = other.m_func;
+    }
+
+    template <typename SharedPtrType>
+    slot &track(SharedPtrType const &sharedptr)
+    {
+        auto weak = typename shared_ptr_traits<SharedPtrType>::weak_type(sharedptr);
+        m_weak = std::make_unique<weak_ptr_continer_imp<decltype(weak)>>(weak);
+        m_tracked = true;
+        return *this;
+    }
+
+    template <typename... _Args>
+    bool operator()(_Args &&... args)
+    {
+        shared_ptr_continer sharad;
+        if (m_tracked)
+        {
+            sharad = m_weak->lock();
+            if (not sharad)
+            {
+                return false;
+            }
+        }
+        if (!m_func)
+        {
+            return false;
+        }
+        m_func(std::forward<_Args>(args)...);
+        return true;
+    }
+
+private:
+    SlotFunc m_func;
+    std::unique_ptr<weak_ptr_continer_base> m_weak;
+    bool m_tracked = false;
+};
+
+} // namespace detail
+
+template <typename... signature>
+class sigslot;
+
+template <typename R, typename... ArgTypes>
+class sigslot<R(ArgTypes...)>
+{
+public:
+    using SlotType = detail::slot<R(ArgTypes...)>;
+    using SlotID = std::weak_ptr<SlotType>;
+
+    SlotID connect(SlotType &slot)
+    {
+        auto const s = std::make_shared<SlotType>(slot);
+        m_slots.push_back(s);
+        return s;
+    }
+
+    void disconnect(SlotID const &s)
+    {
+        m_slots.remove_if([&s](SlotType const &slot) {
+            return slot == s.lock();
         });
     }
 
     template <typename... _Args>
     void operator()(_Args &&... args)
     {
-        m_funcs.remove_if([&args...](SlotType slot) {
-            if (slot.second.lock())
-            {
-                (*slot.first)(std::forward<_Args>(args)...);
-                return false;
-            }
-            return true;
+        m_slots.remove_if([&args...](std::shared_ptr<SlotType> const &s) {
+            return !(*s)(std::forward<_Args>(args)...);
         });
     }
 
 private:
-    std::list<SlotType> m_funcs;
+    std::list<std::shared_ptr<SlotType>> m_slots;
 };
+
+} // namespace utils
--- test.cpp
+++ test.cpp
@@ -5,52 +5,7 @@
 
 using namespace std;
 
-class observer
-{
-public:
-    static observer *instance()
-    {
-        static observer inst;
-        return &inst;
-    }
-
-    sigslot<int>::SlotID connect(sigslot<int>::SlotFunc slot)
-    {
-        return m_sig.connect(std::move(slot));
-    }
-
-    void notify(int n)
-    {
-        m_sig(n);
-    }
-
-private:
-    sigslot<int> m_sig;
-};
-
-class subject : public enable_shared_from_this<subject>
-{
-public:
-    void fun(int a)
-    {
-        cout << "in subject::fun a=" << a << endl;
-    }
-
-    void reg()
-    {
-        m_id = observer::instance()->connect([self = (std::weak_ptr<subject>)shared_from_this()](int s) {
-            if (auto shared_self = self.lock())
-            {
-                shared_self->fun(s);
-            }
-        });
-    }
-
-private:
-    sigslot<int>::SlotID m_id;
-};
-
-class A : public trackable
+class A
 {
 public:
     void haha(int c)
@@ -63,12 +18,17 @@ int main(int, char **)
 {
 
     //   observer::instance()->notify(99);
-    sigslot<int> sig;
+    utils::sigslot<void(int)> sig;
     //{
     auto a = std::make_shared<A>();
-    auto h = sig.connect(std::bind(&A::haha, a.get(), std::placeholders::_1), a);
+
+    //cout << slot(66) << endl;
+
+    sig.connect(utils::sigslot<void(int)>::SlotType(std::bind(&A::haha, a.get(), std::placeholders::_1)).track(a));
+    //a.reset();
+    //auto h = sig.connect(slot);
     //}
-    a.reset();
+    //a.reset();
     //sig.disconnect(h);
     sig(99);
 
